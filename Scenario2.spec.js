26c30a6e1db98da2866ab6d719678bedd7d63aa3
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-10-06 11:32:48 +0530

    Scenario2.spec.js

--- Scenario2.spec.js
+++ Scenario2.spec.js
@@ -4,23 +4,16 @@ test('Scenario2', async({page}) => {
     
     
     await page.goto('https://www.lambdatest.com/selenium-playground/')
-    await page.click("//a[text()='Drag & Drop Sliders']")
+    await page.click("//a[text()='Drag & Drop Sliders']") 
     
-    //await page.locator("//output[@id='rangeSuccess' and text()=95]")
-   // await page.click("//button[text()='Get Checked Value']")
-    //await page.check("//p[@id='message']")
-    const sourceSelector = "//output[@id='rangeSuccess' and text()=15]"; 
-   const destinationSelector = "//output[@id='rangeSuccess' and text()=95]"; // Replace with the actual selector for the ending element (e.g., the element with 95)
+    const slide = await page.locator("//input[@type='range' and @value='15']")
+    let temp = 15
+    while(temp!=95){
+          let expected = await page.locator("//output[@id='rangeSuccess']").getByText()
+          await slide.press('ArrowRight')
+          temp++
 
-  // Get the source and destination elements
-  const sourceElement = await page.$(sourceSelector);
-  const destinationElement = await page.$(destinationSelector);
-
-  // Drag and drop from source to destination
-  await sourceElement.hover(destinationElement)
-
-
-
-    await page.waitForTimeout(10000)
+   }
+    await page.waitForTimeout(5000)
 
 })
