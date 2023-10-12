ae29f5ca85b230edf7388d6222ec94fb498f1687
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-10-12 10:46:00 +0530

    Scenario1.spec.js

--- Scenario1.spec.js
+++ Scenario1.spec.js
@@ -1,29 +1,15 @@
-const{test,expert, expect} = require('@playwright/test')
+const {test,expect}=require('@playwright/test')
+test('Scenario1',async({page})=>{
+await page.goto('https://www.lambdatest.com/selenium-playground')
+await page.click("//a[text()='Simple Form Demo']")
+const url = await expect(page).toHaveURL('https://www.lambdatest.com/selenium-playground/simple-form-demo');
+console.log(url)
+//await expect(page).toHaveTitle('Simple Form Demo')
+const b=await page.locator("//input[@id='user-message']").fill("Meghana")
+await page.click("//button[text()='Get Checked Value']")
 
-test('Scenario1', async({page}) => {
-    
-    
-    await page.goto('https://www.lambdatest.com/selenium-playground/')
-    await page.click("//a[text()='Simple Form Demo']")
-    //const url = await expect(page).toHaveURL('https://www.lambdatest.com/selenium-playground/simple-form-demo');
-    const URL = page.url()
-    if(URL.includes('simple-form-demo')){
-        console.log('simple-form-demo')
-    }
-    else{
-        console.log('simple-form-demo is not matched')
-    }
-    //console.log(url)
-    const msg = 'Welcome to Sindhuja Pathuri for Lambda Test'
-    await page.locator("//input[@id='user-message']").fill(msg)
-    await page.click("//button[text()='Get Checked Value']")
-    const msgs = await page.locator("//p[@id='message']").innerText(msg)
-    if(msg==msgs){
-        console.log(msg)
-    }
-    else{
-        console.log('Message is not matched')
-    }
-    await page.waitForTimeout(10000)
+await expect(await page.locator("//p[@id='message']")).toHaveText('Meghana')
 
+//console.log(b)
+await page.waitForTimeout(5000)
 })
